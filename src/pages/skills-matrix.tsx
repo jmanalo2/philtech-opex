c15d1609ca7d02691d35562a27dec2bfc17c8410
jmanalo2 <jayarmanalo1014@gmail.com>
2026-01-28 11:34:38 +0800

    Add files via upload

---
+++ src/pages/skills-matrix.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@/components/Layout";
+import { SkillsMatrixModule } from "@/components/modules/SkillsMatrixModule";
+
+export default function SkillsMatrixPage() {
+  return (
+    <Layout>
+      <SkillsMatrixModule />
+    </Layout>
+  );
+}
